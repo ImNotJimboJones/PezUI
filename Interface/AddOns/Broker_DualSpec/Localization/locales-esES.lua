3f86f0d9afe8dce356662d3edcf4bef8995d4353
Spence DiNicolantonio <ImNotJimboJones@mac.com>
2012-03-25 15:44:06 -0400

    Updated Data Broker Dual Specialisation

---
+++ Interface/AddOns/Broker_DualSpec/Localization/locales-esES.lua
@@ -0,0 +1,36 @@
+﻿-- ********************************************************************************
+-- Data Broker Dual Specilisation (Broker_DualSpec)
+-- A dual specialisation switch for Data Broker.
+-- By: Shenton
+--
+-- Locales-esES.lua
+-- ********************************************************************************
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Broker_DualSpec", "esES");
+
+if L then
+L["ADDON_NAME"] = "DualSpec"
+L["CLOSE"] = "Cerrar"
+L["COMMAND_USAGE_1"] = "Comandos, \"/ds\", \"/bds\", \"/brokerdualspec\""
+L["COMMAND_USAGE_2"] = "Sin comandos adicionales, solo cambiará tu especialización"
+L["COMMAND_USAGE_3"] = "\"/ds show\", mostrar icono en el minimapa, \"/ds list\", listado de tus equipaciones"
+L["COMMAND_USAGE_4"] = "\"/ds <primary|secondary> <gear set>\", asignará la especialización a la equipación seleccionada"
+L["CURRENT_SPEC"] = "Especialización actual"
+L["DISPLAY_SPEC_NAME"] = "Mostrar nombre de especialización"
+L["DISPLAY_SPEC_POINTS"] = "Mostrar los puntos de talentos"
+L["GEAR_NOT_EXISTS"] = "Esa equipación no existe, usa \"/ds list\" para verlas"
+L["GEAR_SETS"] = "Equipaciones"
+L["HIDE_MINIMAP"] = "El icono del minimapa está oculto. Si quieres mostrarlo utilizar: \"/ds show\" (o /bds /brokerdualspec)"
+L["IN_COMBAT"] = "Mientras estás en combate no puedes cambiar de especialización"
+L["NO_GEAR_DEFINED"] = "No has elegido la equipación de esa especialización"
+L["NO_GEAR_SETS"] = "No tienes una equipación establecida"
+L["NOT_AVAILABLE"] = "No disponible"
+L["OPTIONS"] = "Opciones"
+L["PRIMARY_SPEC"] = "Primaria"
+L["SECONDARY_SPEC"] = "Secundaria"
+L["SHOW_HIDE_MINIMAP"] = "Mostrar/ocultar icono en el minimapa"
+L["SWITCH_GEAR_TOO"] = "Cambiar de equipación también"
+L["SWITCH_TO"] = "Cambiar a"
+L["TOOLTIP_TIPS"] = "%sClick izquierdo:%s Cambiar de especialización y equipo si está permitido\n%sClick derecho:%s Muestra el menú de configuración";
+L["WITH_GEAR_SET"] = "Con equipación"
+end
---
+++ Interface/AddOns/Broker_DualSpec/Localization/locales-zhCN.lua
@@ -0,0 +1,36 @@
+﻿-- ********************************************************************************
+-- Data Broker Dual Specilisation (Broker_DualSpec)
+-- A dual specialisation switch for Data Broker.
+-- By: Shenton
+--
+-- Locales-zhCN.lua
+-- ********************************************************************************
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Broker_DualSpec", "zhCN");
+
+if L then
+-- L["ADDON_NAME"] = "DualSpec"
+L["CLOSE"] = "关闭" -- Needs review
+L["COMMAND_USAGE_1"] = "命令行, \"/ds\", \"/bds\", \"/brokerdualspec\"" -- Needs review
+-- L["COMMAND_USAGE_2"] = "With no argument it will change your specialisation"
+-- L["COMMAND_USAGE_3"] = "\"/ds show\", show the minimap icon, \"/ds list\", list your gear sets"
+-- L["COMMAND_USAGE_4"] = "\"/ds <primary|secondary> <gear set>\", will set the given specialisation with the given gear set"
+L["CURRENT_SPEC"] = "当前天赋" -- Needs review
+L["DISPLAY_SPEC_NAME"] = "显示天赋名字" -- Needs review
+L["DISPLAY_SPEC_POINTS"] = "显示天赋点数" -- Needs review
+L["GEAR_NOT_EXISTS"] = "装备套装不存在,使用 \"/ds list\" 来获取" -- Needs review
+L["GEAR_SETS"] = "状态套装" -- Needs review
+-- L["HIDE_MINIMAP"] = "Minimap icon is hidden if you want to show it back use: \"/ds show\" (or /bds /brokerdualspec)"
+L["IN_COMBAT"] = "你在战斗中不能切换天赋" -- Needs review
+L["NO_GEAR_DEFINED"] = "切换套装已启用但没有定义套装" -- Needs review
+L["NO_GEAR_SETS"] = "你没有定义套装" -- Needs review
+L["NOT_AVAILABLE"] = "不可哟个" -- Needs review
+L["OPTIONS"] = "选项" -- Needs review
+L["PRIMARY_SPEC"] = "主天赋" -- Needs review
+L["SECONDARY_SPEC"] = "副天赋" -- Needs review
+L["SHOW_HIDE_MINIMAP"] = "显示/隐藏小地图按钮" -- Needs review
+L["SWITCH_GEAR_TOO"] = "同时切换装备" -- Needs review
+L["SWITCH_TO"] = "切换到" -- Needs review
+L["TOOLTIP_TIPS"] = "%s左键点击:%s 切换天赋和套装\n%s右键点击:%s 显示配置面板" -- Needs review
+L["WITH_GEAR_SET"] = "同时切换套装" -- Needs review
+end
