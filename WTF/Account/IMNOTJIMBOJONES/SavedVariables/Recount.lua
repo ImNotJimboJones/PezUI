4b2318ac9e1f020b5857d4557fdbf4c98bef4e09
Spence <ImNotJimboJones@mac.com>
2012-08-11 17:01:56 -0400

    Tweaked size/position of damage meters (Recount and Skada)

--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/Recount.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/Recount.lua
@@ -7,18 +7,18 @@ RecountDB = {
 		["Default"] = {
 			["GraphWindowY"] = 0,
 			["MainWindow"] = {
+				["Position"] = {
+					["y"] = -385.4982511124019,
+					["x"] = -279.649817804427,
+					["w"] = 226.0000389807265,
+					["h"] = 151.9842736704136,
+				},
+				["ShowScrollbar"] = false,
 				["Buttons"] = {
 					["CloseButton"] = false,
 					["LeftButton"] = false,
 					["ConfigButton"] = false,
 					["RightButton"] = false,
-				},
-				["ShowScrollbar"] = false,
-				["Position"] = {
-					["y"] = -387.633328513644,
-					["x"] = -279.9998612316849,
-					["w"] = 226.0703826423338,
-					["h"] = 150.8973432993602,
 				},
 				["HideTotalBar"] = false,
 				["BarText"] = {
@@ -26,15 +26,16 @@ RecountDB = {
 				},
 				["RowHeight"] = 16,
 			},
+			["MainWindowMode"] = 2,
 			["MaxFights"] = 25,
-			["MainWindowHeight"] = 150.8973541259766,
+			["MainWindowHeight"] = 151.9842736704136,
 			["ClampToScreen"] = true,
 			["Colors"] = {
 				["Other Windows"] = {
 					["Title"] = {
-						["r"] = 0.392156862745098,
-						["g"] = 0.392156862745098,
 						["b"] = 0.392156862745098,
+						["g"] = 0.392156862745098,
+						["r"] = 0.392156862745098,
 					},
 				},
 				["Window"] = {
@@ -52,18 +53,18 @@ RecountDB = {
 					},
 					["Total Bar"] = {
 						["a"] = 1,
-						["r"] = 0.7490196078431373,
-						["g"] = 0.7490196078431373,
 						["b"] = 0.7490196078431373,
+						["g"] = 0.7490196078431373,
+						["r"] = 0.7490196078431373,
 					},
 				},
 			},
-			["DetailWindowY"] = -2119318916510730,
-			["DetailWindowX"] = 345005682561372.6,
+			["DetailWindowY"] = -51.00000376129817,
+			["DetailWindowX"] = 8,
 			["GraphWindowX"] = 0,
 			["Locked"] = true,
 			["Scaling"] = 0.85,
-			["BarTextColorSwap"] = false,
+			["LastInstanceName"] = "Ulduar",
 			["BarTexture"] = "Minimalist",
 			["RealtimeWindows"] = {
 				["Realtime_FPS_FPS"] = {
@@ -88,9 +89,8 @@ RecountDB = {
 				},
 			},
 			["CurDataSet"] = "LastFightData",
-			["LastInstanceName"] = "Firelands",
-			["MainWindowMode"] = 2,
-			["MainWindowWidth"] = 226.0703887939453,
+			["BarTextColorSwap"] = false,
+			["MainWindowWidth"] = 226.0000389807265,
 		},
 		["Pezzer - Burning Blade"] = {
 			["GraphWindowY"] = 0,
@@ -99,38 +99,31 @@ RecountDB = {
 				["BarText"] = {
 					["NumFormat"] = 3,
 				},
+				["Buttons"] = {
+					["ConfigButton"] = false,
+					["CloseButton"] = false,
+					["RightButton"] = false,
+					["LeftButton"] = false,
+				},
 				["Position"] = {
 					["y"] = -411.5000305175781,
 					["h"] = 137,
 					["w"] = 169,
 					["x"] = -311.9999694824219,
 				},
-				["Buttons"] = {
-					["CloseButton"] = false,
-					["ConfigButton"] = false,
-					["RightButton"] = false,
-					["LeftButton"] = false,
-				},
 			},
 			["DetailWindowX"] = 0,
 			["MaxFights"] = 25,
 			["BarTextColorSwap"] = false,
 			["BarTexture"] = "Minimalist",
-			["GraphWindowX"] = 0,
+			["MainWindowHeight"] = 137,
 			["CurDataSet"] = "OverallData",
 			["Colors"] = {
 				["Window"] = {
 					["Title"] = {
-						["r"] = 0.392156862745098,
-						["g"] = 0.392156862745098,
 						["b"] = 0.392156862745098,
-					},
-				},
-				["Other Windows"] = {
-					["Title"] = {
-						["r"] = 0.392156862745098,
 						["g"] = 0.392156862745098,
-						["b"] = 0.392156862745098,
+						["r"] = 0.392156862745098,
 					},
 				},
 				["Bar"] = {
@@ -141,12 +134,19 @@ RecountDB = {
 						["a"] = 1,
 					},
 				},
+				["Other Windows"] = {
+					["Title"] = {
+						["b"] = 0.392156862745098,
+						["g"] = 0.392156862745098,
+						["r"] = 0.392156862745098,
+					},
+				},
 			},
-			["DetailWindowY"] = 0,
+			["GraphWindowX"] = 0,
 			["Locked"] = true,
 			["ClampToScreen"] = true,
 			["MainWindowWidth"] = 168.6666107177734,
-			["MainWindowHeight"] = 137,
+			["DetailWindowY"] = 0,
 		},
 	},
 }
--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/Skada.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/Skada.lua
@@ -7,50 +7,49 @@ SkadaDB = {
 		["Default"] = {
 			["showtotals"] = true,
 			["modules"] = {
-				["ccignoremaintanks"] = true,
 				["ccannounce"] = true,
+				["ccignoremaintanks"] = true,
 			},
 			["windows"] = {
 				{
-					["mode"] = "Damage taken",
-					["barheight"] = 14,
+					["barfont"] = "Arial Narrow",
+					["point"] = "BOTTOM",
 					["barbgcolor"] = {
 						["a"] = 0.6000000238418579,
-						["b"] = 0.3019607843137255,
-						["g"] = 0.3019607843137255,
 						["r"] = 0.3019607843137255,
+						["g"] = 0.3019607843137255,
+						["b"] = 0.3019607843137255,
 					},
 					["barcolor"] = {
-						["b"] = 0.7490196078431373,
-						["g"] = 0.7490196078431373,
 						["r"] = 0.7490196078431373,
+						["g"] = 0.7490196078431373,
+						["b"] = 0.7490196078431373,
 					},
-					["point"] = "BOTTOM",
 					["barslocked"] = true,
 					["background"] = {
+						["height"] = 106.1333103916941,
 						["color"] = {
 							["a"] = 0,
 							["b"] = 0.5019607843137255,
 						},
-						["height"] = 103.6235427856445,
 					},
 					["bartexture"] = "Minimalist",
-					["barwidth"] = 189.8040466308594,
+					["barwidth"] = 188.1307090778795,
 					["barspacing"] = 1,
-					["y"] = 6.350785406539217e-005,
-					["x"] = 280.5475463867188,
+					["y"] = 0.8365733041793972,
+					["x"] = 279.7109085374698,
 					["title"] = {
 						["font"] = "Arial Narrow",
-						["fontsize"] = 13,
 						["color"] = {
 							["a"] = 0,
-							["b"] = 0.3019607843137255,
-							["g"] = 0.1019607843137255,
 							["r"] = 0.1019607843137255,
+							["g"] = 0.1019607843137255,
+							["b"] = 0.3019607843137255,
 						},
 						["texture"] = "Minimalist",
 					},
-					["barfont"] = "Arial Narrow",
+					["barheight"] = 14,
+					["mode"] = "DPS",
 				}, -- [1]
 			},
 			["icon"] = {
