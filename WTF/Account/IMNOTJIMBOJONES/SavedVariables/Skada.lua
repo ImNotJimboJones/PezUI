7806ed6f337302cef39d8735bd5e39f300807c68
Spence <ImNotJimboJones@mac.com>
2012-08-14 21:04:51 -0400

    Disabled class icons in Skada meters

--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/Skada.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/Skada.lua
@@ -7,49 +7,50 @@ SkadaDB = {
 		["Default"] = {
 			["showtotals"] = true,
 			["modules"] = {
-				["ccannounce"] = true,
 				["ccignoremaintanks"] = true,
+				["ccannounce"] = true,
 			},
 			["windows"] = {
 				{
-					["barfont"] = "Arial Narrow",
 					["barheight"] = 14,
+					["classicons"] = false,
+					["barslocked"] = true,
+					["y"] = 0.8365733041793972,
+					["barfont"] = "Arial Narrow",
+					["title"] = {
+						["color"] = {
+							["a"] = 0,
+							["b"] = 0.3019607843137255,
+							["g"] = 0.1019607843137255,
+							["r"] = 0.1019607843137255,
+						},
+						["font"] = "Arial Narrow",
+						["texture"] = "Minimalist",
+					},
 					["barbgcolor"] = {
 						["a"] = 0.6000000238418579,
-						["r"] = 0.3019607843137255,
-						["g"] = 0.3019607843137255,
 						["b"] = 0.3019607843137255,
+						["g"] = 0.3019607843137255,
+						["r"] = 0.3019607843137255,
 					},
+					["mode"] = "DPS",
+					["bartexture"] = "Minimalist",
+					["barwidth"] = 188.1307090778795,
+					["barspacing"] = 1,
+					["point"] = "BOTTOM",
 					["barcolor"] = {
-						["r"] = 0.7490196078431373,
-						["g"] = 0.7490196078431373,
 						["b"] = 0.7490196078431373,
+						["g"] = 0.7490196078431373,
+						["r"] = 0.7490196078431373,
 					},
-					["barslocked"] = true,
 					["background"] = {
-						["height"] = 106.1333103916941,
 						["color"] = {
 							["a"] = 0,
 							["b"] = 0.5019607843137255,
 						},
+						["height"] = 106.1333103916941,
 					},
-					["bartexture"] = "Minimalist",
-					["barwidth"] = 188.1307090778795,
-					["barspacing"] = 1,
-					["y"] = 0.8365733041793972,
 					["x"] = 279.7109085374698,
-					["title"] = {
-						["font"] = "Arial Narrow",
-						["color"] = {
-							["a"] = 0,
-							["r"] = 0.1019607843137255,
-							["g"] = 0.1019607843137255,
-							["b"] = 0.3019607843137255,
-						},
-						["texture"] = "Minimalist",
-					},
-					["point"] = "BOTTOM",
-					["mode"] = "DPS",
 				}, -- [1]
 			},
 			["icon"] = {
