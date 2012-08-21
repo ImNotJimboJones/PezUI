ec9bb640204d8c5f6d17275891a6b6267cd314f8
Spence <ImNotJimboJones@mac.com>
2012-08-21 04:04:29 -0400

    Repositioned recount details window and tweaked Skada  settings

--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/Skada.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/Skada.lua
@@ -7,57 +7,59 @@ SkadaDB = {
 		["Default"] = {
 			["showtotals"] = true,
 			["modules"] = {
-				["ccignoremaintanks"] = true,
 				["ccannounce"] = true,
+				["ccignoremaintanks"] = true,
 			},
 			["windows"] = {
 				{
 					["barheight"] = 14,
 					["classicons"] = false,
 					["barslocked"] = true,
+					["background"] = {
+						["height"] = 106.1333103916941,
+						["color"] = {
+							["a"] = 0,
+							["b"] = 0.5019607843137255,
+						},
+					},
+					["set"] = "total",
 					["y"] = 0.8365733041793972,
-					["barfont"] = "Arial Narrow",
+					["x"] = 279.7109085374698,
 					["title"] = {
 						["color"] = {
 							["a"] = 0,
-							["b"] = 0.3019607843137255,
-							["g"] = 0.1019607843137255,
 							["r"] = 0.1019607843137255,
+							["g"] = 0.1019607843137255,
+							["b"] = 0.3019607843137255,
 						},
 						["font"] = "Arial Narrow",
 						["texture"] = "Minimalist",
 					},
 					["barbgcolor"] = {
 						["a"] = 0.6000000238418579,
-						["b"] = 0.3019607843137255,
-						["g"] = 0.3019607843137255,
 						["r"] = 0.3019607843137255,
+						["g"] = 0.3019607843137255,
+						["b"] = 0.3019607843137255,
 					},
-					["mode"] = "DPS",
 					["bartexture"] = "Minimalist",
 					["barwidth"] = 188.1307090778795,
 					["barspacing"] = 1,
 					["point"] = "BOTTOM",
 					["barcolor"] = {
-						["b"] = 0.7490196078431373,
-						["g"] = 0.7490196078431373,
 						["r"] = 0.7490196078431373,
+						["g"] = 0.7490196078431373,
+						["b"] = 0.7490196078431373,
 					},
-					["background"] = {
-						["color"] = {
-							["a"] = 0,
-							["b"] = 0.5019607843137255,
-						},
-						["height"] = 106.1333103916941,
-					},
-					["x"] = 279.7109085374698,
+					["mode"] = "Damage taken",
+					["barfont"] = "Arial Narrow",
 				}, -- [1]
 			},
 			["icon"] = {
 				["hide"] = true,
 			},
 			["report"] = {
-				["mode"] = "Healing and Absorbs",
+				["set"] = "total",
+				["mode"] = "DPS",
 			},
 			["hidedisables"] = false,
 			["tooltippos"] = "topleft",
