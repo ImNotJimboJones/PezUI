7180f872d307b56bec56e4b8d81fec2b9519661f
Spence <ImNotJimboJones@mac.com>
2012-08-21 04:19:29 -0400

    Repositioned small world map

---
+++ WTF/Account/IMNOTJIMBOJONES/Burning Blade/Pezzer/SavedVariables/Blizzard_BattlefieldMinimap.lua
@@ -0,0 +1,10 @@
+
+BattlefieldMinimapOptions = {
+	["locked"] = true,
+	["opacity"] = 0.699999988079071,
+	["showPlayers"] = true,
+	["position"] = {
+		["y"] = 782.3398652949189,
+		["x"] = 61.64055489132549,
+	},
+}
