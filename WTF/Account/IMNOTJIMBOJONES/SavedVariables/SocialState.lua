ed3213fbb82208de6ed9e0891f7c95797e7b8df1
Spence <ImNotJimboJones@mac.com>
2012-08-25 23:35:40 -0400

    No longer showing GMOTD in Titan Panel's Social plugin

--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/SocialState.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/SocialState.lua
@@ -3,8 +3,9 @@ SocialStateDB = {
 	["RealIDSort"] = "ZONENAME",
 	["GuildSort"] = "ZONENAME",
 	["hide_LDB_totals"] = true,
+	["hide_hintline"] = false,
+	["hide_gmotd"] = true,
 	["tooltip_autohide"] = "0.00001",
 	["hide_LDB_labels"] = false,
-	["hide_gmotd"] = false,
 	["expand_realID"] = false,
 }
