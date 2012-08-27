635f0b83fbd0128d6aed831b50a8fcc389bd0f7d
Spence <ImNotJimboJones@mac.com>
2012-08-27 02:20:48 -0400

    Cleared WhisperWhisper conversations

--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/WhisperWhisper.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/WhisperWhisper.lua
@@ -1,106 +1,64 @@
 
 Broker_WhisperWhisperDB = {
 	["profileKeys"] = {
-		["Pezzer - Burning Blade"] = "Default",
+		["Pezzer - Burning Blade"] = "default",
 	},
 	["profiles"] = {
 		["Default"] = {
 			["flashRight"] = true,
 			["sendColor"] = {
-				["b"] = 210,
-				["g"] = 210,
 				["r"] = 210,
+				["g"] = 210,
+				["b"] = 210,
 			},
 			["flashTop"] = true,
 			["frameLock"] = true,
 			["flashOffCombat"] = false,
-			["flashBottom"] = true,
+			["frameOpts"] = {
+				["offsetx"] = -580.2666473388672,
+				["offsety"] = -78.30596923828125,
+			},
 			["flashLeft"] = true,
-			["altView"] = false,
 			["showFrame"] = false,
 			["flashMaster"] = true,
 			["flashEntireFrame"] = true,
 			["flashDuration"] = 900,
 			["recColor"] = {
-				["b"] = 8,
-				["g"] = 0,
 				["r"] = 255,
+				["g"] = 0,
+				["b"] = 8,
+			},
+			["flashBottom"] = true,
+			["numWhispers"] = 20,
+			["altView"] = false,
+		},
+		["default"] = {
+			["flashRight"] = true,
+			["sendColor"] = {
+				["r"] = 210,
+				["g"] = 210,
+				["b"] = 210,
+			},
+			["flashTop"] = true,
+			["frameLock"] = true,
+			["flashOffCombat"] = false,
+			["altView"] = false,
+			["flashLeft"] = true,
+			["flashBottom"] = true,
+			["showFrame"] = false,
+			["flashMaster"] = true,
+			["flashEntireFrame"] = true,
+			["flashDuration"] = 900,
+			["recColor"] = {
+				["r"] = 255,
+				["g"] = 0,
+				["b"] = 8,
 			},
 			["frameOpts"] = {
 				["offsetx"] = -580.2666473388672,
 				["offsety"] = -78.30596923828125,
 			},
 			["numWhispers"] = 20,
-			["whisperList"] = {
-				{
-					"Lívely", -- [1]
-					{
-						"|cffff0008where is fate?", -- [1]
-					}, -- [2]
-					false, -- [3]
-					false, -- [4]
-				}, -- [1]
-				{
-					"Btchgrip", -- [1]
-					{
-						"|cffff0008too hard with 5 people", -- [1]
-						"|cffff0008I 3 man'ed it with sciro on my priest", -- [2]
-					}, -- [2]
-					false, -- [3]
-					false, -- [4]
-				}, -- [2]
-				{
-					"Vâcano", -- [1]
-					{
-						"|cffd2d2d2thx", -- [1]
-						"|cffff0008ty", -- [2]
-						"|cffff0008correct", -- [3]
-						"|cffd2d2d2 |cffa335ee|Hitem:77171:0:0:0:0:0:0:0:85:0|h[Bladeshatter Treads]|h|r|cffd2d2d2r?", -- [4]
-						"|cffff0008yes", -- [5]
-						"|cffd2d2d2 |cffa335ee|Hitem:77322:0:0:0:0:0:0:0:85:0|h[Bracers of Manifold Pockets]|h|r|cffd2d2d2r?", -- [6]
-						"|cffff0008i need leather agility wrist and tanking plate boots", -- [7]
-						"|cffff0008ok", -- [8]
-						"|cffd2d2d2what do you need?", -- [9]
-						"|cffd2d2d2k", -- [10]
-					}, -- [2]
-					true, -- [3]
-					false, -- [4]
-				}, -- [3]
-				{
-					"Shiftsalot", -- [1]
-					{
-						"|cffff0008ty", -- [1]
-						"|cffd2d2d2thnx", -- [2]
-						"|cffff0008ok", -- [3]
-						"|cffd2d2d2sure", -- [4]
-						"|cffff0008let me switch toons to get the gold. brb", -- [5]
-						"|cffff0008 |cffa335ee|Hitem:77173:0:0:0:0:0:0:0:85:0|h[Rooftop Griptoes]|h|r|cffff0008r", -- [6]
-						"|cffff0008one sec let me tell ya", -- [7]
-						"|cffd2d2d2what kind?", -- [8]
-						"|cffd2d2d2you got it", -- [9]
-						"|cffff0008id pay 2k for boots", -- [10]
-					}, -- [2]
-					false, -- [3]
-					false, -- [4]
-				}, -- [4]
-				{
-					"Mytios", -- [1]
-					{
-						"|cffff0008WHAT? LOL i wish", -- [1]
-						"|cffd2d2d2ive got 10k valor to burn lol", -- [2]
-						"|cffd2d2d2to be honest, price doesnt matter", -- [3]
-						"|cffff0008i think ill wait, but if i were you id sell for more than 1k :P 1.5k is reasonable", -- [4]
-						"|cffff0008haha ill think about it. gimme a min.", -- [5]
-						"|cffd2d2d2fuck it... 1k and they're yours :P", -- [6]
-						"|cffff0008im good lol", -- [7]
-						"|cffd2d2d2hows that?", -- [8]
-						"|cffd2d2d2last guy gave me 2k", -- [9]
-						"|cffff0008you said cheap i assume you know your price lol", -- [10]
-					}, -- [2]
-					false, -- [3]
-					false, -- [4]
-				}, -- [5]
-			},
 		},
 	},
 }
