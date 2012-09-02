b428c08cb06685b4ca05d79dc95e255cb29caf13
Spence <ImNotJimboJones@mac.com>
2012-09-02 13:22:38 -0400

    Configured/positioned Ion pet bar (may be revised)

---
+++ WTF/Account/IMNOTJIMBOJONES/Burning Blade/Pezzer/SavedVariables/Ion-PetBar.lua
@@ -0,0 +1,152 @@
+
+IonPetCDB = {
+	["petbars"] = {
+		{
+			["prowl"] = false,
+			["spellGlow"] = true,
+			["cdAlpha"] = false,
+			["auraText"] = false,
+			["tooltips"] = true,
+			["extrabar"] = false,
+			["homestate"] = true,
+			["downClicks"] = false,
+			["shift"] = false,
+			["barLockShift"] = false,
+			["spellGlowDef"] = true,
+			["pet"] = false,
+			["conceal"] = false,
+			["customRange"] = false,
+			["macroText"] = true,
+			["dualSpec"] = false,
+			["alt"] = false,
+			["combat"] = false,
+			["group"] = false,
+			["rangeInd"] = true,
+			["barLock"] = false,
+			["cdText"] = false,
+			["tooltipsCombat"] = false,
+			["countText"] = true,
+			["paged"] = false,
+			["barLockAlt"] = false,
+			["vehicle"] = false,
+			["upClicks"] = true,
+			["override"] = false,
+			["tooltipsEnhanced"] = true,
+			["customNames"] = false,
+			["fishing"] = false,
+			["auraInd"] = false,
+			["barLockCtrl"] = false,
+			["bindText"] = true,
+			["possess"] = false,
+			["remap"] = false,
+			["custom"] = false,
+			["reaction"] = false,
+			["spellGlowAlt"] = false,
+			["stealth"] = false,
+			["ctrl"] = false,
+			["stance"] = false,
+		}, -- [1]
+	},
+	["petbtns"] = {
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["data"] = {
+			},
+		}, -- [1]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["data"] = {
+			},
+		}, -- [2]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["data"] = {
+			},
+		}, -- [3]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["data"] = {
+			},
+		}, -- [4]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["data"] = {
+			},
+		}, -- [5]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["data"] = {
+			},
+		}, -- [6]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["data"] = {
+			},
+		}, -- [7]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["data"] = {
+			},
+		}, -- [8]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["data"] = {
+			},
+		}, -- [9]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["data"] = {
+			},
+		}, -- [10]
+	},
+}
---
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/Ion-PetBar.lua
@@ -0,0 +1,178 @@
+
+IonPetGDB = {
+	["petbars"] = {
+		{
+			["arcLength"] = 359,
+			["objectStrata"] = "LOW",
+			["snapToPoint"] = false,
+			["snapTo"] = false,
+			["bindColor"] = "1;1;1;1",
+			["snapToPad"] = 0,
+			["hidestates"] = ":pet0:",
+			["y"] = 76.00001525878906,
+			["x"] = -29.699951171875,
+			["name"] = "Pet Bar 1",
+			["padH"] = 17,
+			["barStrata"] = "MEDIUM",
+			["shape"] = 1,
+			["auracolor1"] = "0;0.82;0;1",
+			["macroColor"] = "1;1;1;1",
+			["scale"] = 0.5,
+			["cdcolor2"] = "1;0.1;0.1;1",
+			["padV"] = 0,
+			["debuffcolor"] = "0.8;0;0;1",
+			["fadeSpeed"] = 0.5,
+			["columns"] = 2,
+			["countColor"] = "1;1;1;1",
+			["alphaUp"] = "Combat+Mouseover",
+			["buffcolor"] = "0;0.8;0;1",
+			["alpha"] = 0.45,
+			["arcStart"] = 0,
+			["rangecolor"] = "0.7;0.15;0.15;1",
+			["objectList"] = "1;2;3;4;5;6;7",
+			["snapToFrame"] = false,
+			["point"] = "BOTTOMRIGHT",
+			["auracolor2"] = "1;0.1;0.1;1",
+			["showGrid"] = false,
+			["cdcolor1"] = "1;0.82;0;1",
+			["autoHide"] = false,
+		}, -- [1]
+	},
+	["firstRun"] = false,
+	["petbtns"] = {
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["config"] = {
+				["stored"] = false,
+			},
+		}, -- [1]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["config"] = {
+				["stored"] = false,
+			},
+		}, -- [2]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["config"] = {
+				["stored"] = false,
+			},
+		}, -- [3]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["config"] = {
+				["stored"] = false,
+			},
+		}, -- [4]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["config"] = {
+				["stored"] = false,
+			},
+		}, -- [5]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["config"] = {
+				["stored"] = false,
+			},
+		}, -- [6]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["config"] = {
+				["stored"] = false,
+			},
+		}, -- [7]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["config"] = {
+				["scale"] = 1,
+				["YOffset"] = 0,
+				["anchorDelay"] = false,
+				["anchoredBar"] = false,
+				["clickAnchor"] = false,
+				["stored"] = false,
+				["mouseAnchor"] = false,
+				["target"] = "none",
+				["XOffset"] = 0,
+			},
+		}, -- [8]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["config"] = {
+				["scale"] = 1,
+				["YOffset"] = 0,
+				["anchorDelay"] = false,
+				["anchoredBar"] = false,
+				["clickAnchor"] = false,
+				["stored"] = false,
+				["mouseAnchor"] = false,
+				["target"] = "none",
+				["XOffset"] = 0,
+			},
+		}, -- [9]
+		{
+			["keys"] = {
+				["hotKeyText"] = ":",
+				["hotKeyLock"] = false,
+				["hotKeys"] = ":",
+				["hotKeyPri"] = false,
+			},
+			["config"] = {
+				["scale"] = 1,
+				["YOffset"] = 0,
+				["anchorDelay"] = false,
+				["anchoredBar"] = false,
+				["clickAnchor"] = false,
+				["stored"] = false,
+				["mouseAnchor"] = false,
+				["target"] = "none",
+				["XOffset"] = 0,
+			},
+		}, -- [10]
+	},
+}
