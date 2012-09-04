3a484ad35600dcf057d742b2204af448345fa5bc
Spence <ImNotJimboJones@mac.com>
2012-09-04 00:14:55 -0400

    Removed unused PitBull plugin PitBull4_PetTLBar

--- Interface/AddOns/PitBull4_PetTTLBar/localizations/enUS.lua
+++
@@ -1,5 +0,0 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("PitBull4_PetTTLBar", "enUS", true)
-
-L["Pet Lifetime bar"] = "Pet Lifetime bar"
-L["Show a bar indicating the remaining lifetime of your temporary pet"] = "Show a bar indicating the remaining lifetime of your temporary pet"
-
--- Interface/AddOns/PitBull4_PetTTLBar/localizations/esES.lua
+++
@@ -1,8 +0,0 @@
--- This file is automagically generated.
--- Please visit http://www.wowace.com/projects/pitbull4-pet-ttl-bar/localization
--- if you wish to help with the translation.
-
-local L = LibStub("AceLocale-3.0"):NewLocale("PitBull4_PetTTLBar", "esES")
-if not L then return end
-
-
