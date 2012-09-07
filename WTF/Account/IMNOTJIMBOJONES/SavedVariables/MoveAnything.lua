ff4076ec42636e6387d6c41aa74695f0a79ca305
Spence <ImNotJimboJones@mac.com>
2012-09-07 00:28:37 -0400

    Configured some UI frames (not all) with MoveAnything
      - Bag frames positioned and scaled to 90%
      - Bank and Guild Bank frames scaled to 90%
      - Vehicle seat indicator and durability indicator positioned
      - Battle.net popup message frame positioned

---
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/MoveAnything.lua
@@ -0,0 +1,63 @@
+
+MADB = {
+	["characters"] = {
+	},
+	["frameListRows"] = 18,
+	["tooltips"] = true,
+	["profiles"] = {
+		["default"] = {
+			["name"] = "default",
+			["frames"] = {
+				["BNToastFrame"] = {
+					["name"] = "BNToastFrame",
+					["pos"] = {
+						"BOTTOMLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						20, -- [4]
+						283, -- [5]
+					},
+				},
+				["DurabilityFrame"] = {
+					["name"] = "DurabilityFrame",
+					["pos"] = {
+						"BOTTOMLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						55, -- [4]
+						283, -- [5]
+					},
+				},
+				["VehicleSeatIndicator"] = {
+					["name"] = "VehicleSeatIndicator",
+					["pos"] = {
+						"BOTTOMLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						20, -- [4]
+						283, -- [5]
+					},
+				},
+				["BagsMover"] = {
+					["scale"] = 0.8999999761581421,
+					["name"] = "BagsMover",
+					["pos"] = {
+						"BOTTOMRIGHT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMRIGHT", -- [3]
+						-22.22222281091009, -- [4]
+						314.4444527743777, -- [5]
+					},
+				},
+				["BankFrame"] = {
+					["scale"] = 0.8999999761581421,
+					["name"] = "BankFrame",
+				},
+				["GuildBankFrame"] = {
+					["scale"] = 0.8999999761581421,
+					["name"] = "GuildBankFrame",
+				},
+			},
+		},
+	},
+}
