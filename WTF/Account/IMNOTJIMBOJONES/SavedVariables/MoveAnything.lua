771e560c12a1275033c9bae5a66be9f56e3c58e1
Spence <ImNotJimboJones@mac.com>
2012-09-07 11:56:04 -0400

    Used MoveAnything to hide and move frames
      - player debuffs hidden
      - raid frames hidden
      - boss health frames hidden
      - custom raid manager enabled
      - raid manager toggle button positioned

--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/MoveAnything.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/MoveAnything.lua
@@ -8,26 +8,6 @@ MADB = {
 		["default"] = {
 			["name"] = "default",
 			["frames"] = {
-				["BNToastFrame"] = {
-					["name"] = "BNToastFrame",
-					["pos"] = {
-						"BOTTOMLEFT", -- [1]
-						"UIParent", -- [2]
-						"BOTTOMLEFT", -- [3]
-						20, -- [4]
-						283, -- [5]
-					},
-				},
-				["DurabilityFrame"] = {
-					["name"] = "DurabilityFrame",
-					["pos"] = {
-						"BOTTOMLEFT", -- [1]
-						"UIParent", -- [2]
-						"BOTTOMLEFT", -- [3]
-						55, -- [4]
-						283, -- [5]
-					},
-				},
 				["VehicleSeatIndicator"] = {
 					["name"] = "VehicleSeatIndicator",
 					["pos"] = {
@@ -36,6 +16,39 @@ MADB = {
 						"BOTTOMLEFT", -- [3]
 						20, -- [4]
 						283, -- [5]
+					},
+				},
+				["BankFrame"] = {
+					["name"] = "BankFrame",
+					["scale"] = 0.8999999761581421,
+				},
+				["Boss1TargetFrame"] = {
+					["name"] = "Boss1TargetFrame",
+					["pos"] = {
+						"TOPRIGHT", -- [1]
+						"UIParent", -- [2]
+						"TOPRIGHT", -- [3]
+						55.00007152557, -- [4]
+						-235.9997970064547, -- [5]
+					},
+					["hidden"] = true,
+					["unit"] = "boss1",
+				},
+				["Boss2TargetFrame"] = {
+					["name"] = "Boss2TargetFrame",
+					["hidden"] = true,
+					["unit"] = "boss2",
+				},
+				["Boss5TargetFrame"] = {
+					["name"] = "Boss5TargetFrame",
+					["unit"] = "boss5",
+					["hidden"] = true,
+					["pos"] = {
+						"TOPLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						2383, -- [4]
+						684.0001831054688, -- [5]
 					},
 				},
 				["BagsMover"] = {
@@ -49,13 +62,109 @@ MADB = {
 						314.4444527743777, -- [5]
 					},
 				},
-				["BankFrame"] = {
-					["scale"] = 0.8999999761581421,
-					["name"] = "BankFrame",
+				["CompactRaidGroup8"] = {
+					["hidden"] = true,
+					["name"] = "CompactRaidGroup8",
+				},
+				["Boss3TargetFrame"] = {
+					["name"] = "Boss3TargetFrame",
+					["unit"] = "boss3",
+					["hidden"] = true,
+					["pos"] = {
+						"TOPLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						2383, -- [4]
+						944.0000610351563, -- [5]
+					},
+				},
+				["DurabilityFrame"] = {
+					["name"] = "DurabilityFrame",
+					["pos"] = {
+						"BOTTOMLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						55, -- [4]
+						283, -- [5]
+					},
+				},
+				["RaidUnitFramesManagerMover"] = {
+					["name"] = "RaidUnitFramesManagerMover",
+					["pos"] = {
+						"TOPLEFT", -- [1]
+						"UIParent", -- [2]
+						"TOPLEFT", -- [3]
+						-6.999999523162842, -- [4]
+						-139.9998779296875, -- [5]
+					},
+				},
+				["CompactRaidGroup3"] = {
+					["hidden"] = true,
+					["name"] = "CompactRaidGroup3",
+				},
+				["BNToastFrame"] = {
+					["name"] = "BNToastFrame",
+					["pos"] = {
+						"BOTTOMLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						20, -- [4]
+						283, -- [5]
+					},
+				},
+				["CompactRaidGroup4"] = {
+					["hidden"] = true,
+					["name"] = "CompactRaidGroup4",
 				},
 				["GuildBankFrame"] = {
 					["scale"] = 0.8999999761581421,
 					["name"] = "GuildBankFrame",
+				},
+				["CompactRaidGroup6"] = {
+					["hidden"] = true,
+					["name"] = "CompactRaidGroup6",
+				},
+				["CompactRaidGroup2"] = {
+					["hidden"] = true,
+					["name"] = "CompactRaidGroup2",
+				},
+				["CompactRaidGroup5"] = {
+					["hidden"] = true,
+					["name"] = "CompactRaidGroup5",
+				},
+				["MACompactRaidFrameManagerToggleButton"] = {
+					["name"] = "MACompactRaidFrameManagerToggleButton",
+					["pos"] = {
+						"TOPLEFT", -- [1]
+						"UIParent", -- [2]
+						"TOPLEFT", -- [3]
+						-5, -- [4]
+						-256.0000610351563, -- [5]
+					},
+				},
+				["CompactRaidGroup1"] = {
+					["hidden"] = true,
+					["name"] = "CompactRaidGroup1",
+				},
+				["PlayerDebuffsMover"] = {
+					["hidden"] = true,
+					["name"] = "PlayerDebuffsMover",
+				},
+				["Boss4TargetFrame"] = {
+					["name"] = "Boss4TargetFrame",
+					["unit"] = "boss4",
+					["hidden"] = true,
+					["pos"] = {
+						"TOPLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						2383, -- [4]
+						814.0001831054688, -- [5]
+					},
+				},
+				["CompactRaidGroup7"] = {
+					["hidden"] = true,
+					["name"] = "CompactRaidGroup7",
 				},
 			},
 		},
