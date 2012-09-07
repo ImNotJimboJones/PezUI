07c22295b3978b38da99047db33a390a70525b8b
Spence <ImNotJimboJones@mac.com>
2012-09-07 14:25:34 -0400

    Enabled Critline splash frame and heal tracking (also reset records)

--- WTF/Account/IMNOTJIMBOJONES/Burning Blade/Pezzer/SavedVariables/Critline.lua
+++ WTF/Account/IMNOTJIMBOJONES/Burning Blade/Pezzer/SavedVariables/Critline.lua
@@ -16,319 +16,17 @@ CritlinePerCharDB = {
 	["profiles"] = {
 		["Pezzer - Burning Blade"] = {
 			["spells"] = {
-				["dmg"] = {
-					[107821] = {
-						[2] = {
-							["normal"] = {
-								["target"] = "Asira Dawnslayer",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 1197,
-							},
-							["crit"] = {
-								["target"] = "Asira Dawnslayer",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 1849,
-							},
-						},
-					},
-					[1978] = {
-						[2] = {
-							["crit"] = {
-								["target"] = "Murozond",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 10729,
-							},
-							["normal"] = {
-								["target"] = "Echo of Baine",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 5042,
-							},
-						},
-					},
-					[105006] = {
-						{
-							["normal"] = {
-								["target"] = "Mannoroth",
-								["isPvPTarget"] = false,
-								["targetLevel"] = -1,
-								["amount"] = 32400,
-							},
-						}, -- [1]
-					},
-					[109724] = {
-						{
-							["crit"] = {
-								["target"] = "Echo of Baine",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 27839,
-							},
-							["normal"] = {
-								["target"] = "Echo of Baine",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 14813,
-							},
-						}, -- [1]
-					},
-					[77767] = {
-						{
-							["normal"] = {
-								["target"] = "Murozond",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 19273,
-							},
-							["crit"] = {
-								["target"] = "Murozond",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 39833,
-							},
-						}, -- [1]
-					},
-					[6603] = {
-						{
-							["crit"] = {
-								["target"] = "Core Rager",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 62,
-								["amount"] = 12713,
-							},
-							["normal"] = {
-								["target"] = "Flamewaker Healer",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 60,
-								["amount"] = 5922,
-							},
-						}, -- [1]
-					},
-					[13795] = {
-						[2] = {
-							["normal"] = {
-								["target"] = "Abyssal Doombringer",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 86,
-								["amount"] = 2105,
-							},
-							["crit"] = {
-								["target"] = "Archbishop Benedictus",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 3176,
-							},
-						},
-					},
-					[3044] = {
-						{
-							["crit"] = {
-								["target"] = "Echo of Baine",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 44434,
-							},
-							["normal"] = {
-								["target"] = "Echo of Baine",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 21945,
-							},
-						}, -- [1]
-					},
-					[53351] = {
-						{
-							["crit"] = {
-								["target"] = "Murozond",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 69468,
-							},
-							["normal"] = {
-								["target"] = "Murozond",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 32429,
-							},
-						}, -- [1]
-					},
-					[3674] = {
-						[2] = {
-							["normal"] = {
-								["target"] = "Mannoroth",
-								["isPvPTarget"] = false,
-								["targetLevel"] = -1,
-								["amount"] = 5066,
-							},
-							["crit"] = {
-								["target"] = "Mannoroth",
-								["isPvPTarget"] = false,
-								["targetLevel"] = -1,
-								["amount"] = 10586,
-							},
-						},
-					},
-					[88453] = {
-						[2] = {
-							["crit"] = {
-								["target"] = "Highguard Elite",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 86,
-								["amount"] = 10397,
-							},
-							["normal"] = {
-								["target"] = "Infinite Warden",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 85,
-								["amount"] = 4784,
-							},
-						},
-					},
-					[19503] = {
-						{
-							["normal"] = {
-								["target"] = "Flameguard",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 62,
-								["amount"] = 4045,
-							},
-							["crit"] = {
-								["target"] = "Eye of the Legion",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 85,
-								["amount"] = 8394,
-							},
-						}, -- [1]
-					},
-					[13813] = {
-						{
-							["normal"] = {
-								["target"] = "Anub'arak",
-								["isPvPTarget"] = false,
-								["targetLevel"] = -1,
-								["amount"] = 3167,
-							},
-							["crit"] = {
-								["target"] = "Enchanted Magus",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 85,
-								["amount"] = 5781,
-							},
-						}, -- [1]
-						{
-							["crit"] = {
-								["target"] = "Infinite Suppressor",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 85,
-								["amount"] = 5611,
-							},
-							["normal"] = {
-								["target"] = "Infinite Suppressor",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 85,
-								["amount"] = 3632,
-							},
-						}, -- [2]
-					},
-					[99621] = {
-						{
-							["normal"] = {
-								["target"] = "Flamewaker Healer",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 60,
-								["amount"] = 1409,
-							},
-						}, -- [1]
-					},
-					[53301] = {
-						[2] = {
-							["crit"] = {
-								["target"] = "Murozond",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 34024,
-							},
-							["normal"] = {
-								["target"] = "Murozond",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 87,
-								["amount"] = 15890,
-							},
-						},
-					},
-					[75] = {
-						{
-							["crit"] = {
-								["target"] = "Mannoroth",
-								["isPvPTarget"] = false,
-								["targetLevel"] = -1,
-								["amount"] = 20366,
-							},
-							["normal"] = {
-								["target"] = "Mannoroth",
-								["isPvPTarget"] = false,
-								["targetLevel"] = -1,
-								["amount"] = 10293,
-							},
-						}, -- [1]
-					},
-					[103919] = {
-						{
-							["normal"] = {
-								["target"] = "Doomguard Annihilator",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 85,
-								["amount"] = 134764,
-							},
-						}, -- [1]
-					},
-					[83077] = {
-						{
-							["crit"] = {
-								["target"] = "Highguard Elite",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 86,
-								["amount"] = 23289,
-							},
-							["normal"] = {
-								["target"] = "Infinite Suppressor",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 85,
-								["amount"] = 7179,
-							},
-						}, -- [1]
-					},
+				["pet"] = {
 					[105523] = {
 						{
 							["normal"] = {
 								["target"] = "Mannoroth",
 								["isPvPTarget"] = false,
 								["targetLevel"] = -1,
-								["amount"] = 1504887,
+								["amount"] = 1661372,
 							},
 						}, -- [1]
 					},
-					[2643] = {
-						{
-							["crit"] = {
-								["target"] = "Infinite Suppressor",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 85,
-								["amount"] = 23302,
-							},
-							["normal"] = {
-								["target"] = "Time-Twisted Footman",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 85,
-								["amount"] = 11114,
-							},
-						}, -- [1]
-					},
-				},
-				["pet"] = {
 					[17253] = {
 						{
 							["normal"] = {
@@ -342,32 +40,6 @@ CritlinePerCharDB = {
 								["isPvPTarget"] = false,
 								["targetLevel"] = 86,
 								["amount"] = 15794,
-							},
-						}, -- [1]
-					},
-					[105523] = {
-						{
-							["normal"] = {
-								["target"] = "Mannoroth",
-								["isPvPTarget"] = false,
-								["targetLevel"] = -1,
-								["amount"] = 1661372,
-							},
-						}, -- [1]
-					},
-					[53508] = {
-						{
-							["normal"] = {
-								["target"] = "Highguard Elite",
-								["isPvPTarget"] = false,
-								["targetLevel"] = 86,
-								["amount"] = 1591,
-							},
-							["crit"] = {
-								["target"] = "Garr",
-								["isPvPTarget"] = false,
-								["targetLevel"] = -1,
-								["amount"] = 3226,
 							},
 						}, -- [1]
 					},
@@ -387,8 +59,25 @@ CritlinePerCharDB = {
 							},
 						}, -- [1]
 					},
+					[53508] = {
+						{
+							["normal"] = {
+								["target"] = "Highguard Elite",
+								["isPvPTarget"] = false,
+								["targetLevel"] = 86,
+								["amount"] = 1591,
+							},
+							["crit"] = {
+								["target"] = "Garr",
+								["isPvPTarget"] = false,
+								["targetLevel"] = -1,
+								["amount"] = 3226,
+							},
+						}, -- [1]
+					},
 				},
 			},
+			["heal"] = true,
 		},
 	},
 }
--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/Critline.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/Critline.lua
@@ -4,7 +4,18 @@ CritlineDB = {
 		["splash"] = {
 			["profiles"] = {
 				["Default"] = {
-					["enabled"] = false,
+					["duration"] = 5,
+					["spellColor"] = {
+						["g"] = 0,
+						["b"] = 0.7333333333333333,
+					},
+					["fontSize"] = 22,
+					["fontFace"] = "Friz Quadrata TT",
+					["pos"] = {
+						["y"] = 245.9997100830078,
+						["x"] = -255.9996643066406,
+						["point"] = "RIGHT",
+					},
 				},
 			},
 		},
@@ -32,6 +43,8 @@ CritlineDB = {
 	},
 	["profiles"] = {
 		["Default"] = {
+			["oldRecord"] = true,
+			["shortFormat"] = true,
 		},
 	},
 }
