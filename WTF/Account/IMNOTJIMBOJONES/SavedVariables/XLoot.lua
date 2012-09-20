115f81deacfe51f6ce2368cb20eb640ab6240d36
Spence <ImNotJimboJones@mac.com>
2012-09-20 02:33:30 -0400

    Changed XLoot to 100%, increased opacity, and disabled item info display

--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/XLoot.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/XLoot.lua
@@ -28,8 +28,8 @@ XLootDB = {
 							["attach"] = {
 								["self"] = "TOPLEFT",
 								["x"] = 0,
-								["target"] = "TOPLEFT",
 								["y"] = 0,
+								["target"] = "TOPLEFT",
 							},
 							["scale"] = 0.9,
 							["pos"] = {
@@ -45,7 +45,7 @@ XLootDB = {
 		["XLootGroupDB"] = {
 			["profiles"] = {
 				["Default"] = {
-					["faderows"] = false,
+					["nametrunc"] = 30,
 					["AnchorsAway"] = {
 						["roll"] = {
 							["threshold"] = 10000,
@@ -55,8 +55,8 @@ XLootDB = {
 							["attach"] = {
 								["self"] = "TOPLEFT",
 								["x"] = 0,
-								["target"] = "BOTTOMLEFT",
 								["y"] = 0,
+								["target"] = "BOTTOMLEFT",
 							},
 							["scale"] = 1.25,
 							["pos"] = {
@@ -65,7 +65,7 @@ XLootDB = {
 							},
 						},
 					},
-					["nametrunc"] = 30,
+					["faderows"] = false,
 				},
 			},
 		},
