c572111ff0ef491bc585c7e34cd626742df3464a
Spence <ImNotJimboJones@mac.com>
2012-09-22 14:26:14 -0400

    Added, enabled, and configured _NPCScan and _NPCScan.Overlay

---
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/_NPCScan.lua
@@ -0,0 +1,12 @@
+
+_NPCScanOptions = {
+	["Achievements"] = {
+		[2257] = true,
+		[1312] = true,
+		[7439] = true,
+	},
+	["Version"] = "5.0.0.3",
+	["CacheWarnings"] = true,
+	["AchievementsAddFound"] = true,
+	["AlertSoundUnmute"] = true,
+}
