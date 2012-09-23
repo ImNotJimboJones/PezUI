980c5f96638b479daba4a85e8ff08babd0aec1b1
Spence <ImNotJimboJones@mac.com>
2012-09-23 19:43:42 -0400

    Updated Atlas config to avoid dialog at login

--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/Atlas.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/Atlas.lua
@@ -1,6 +1,7 @@
 
 AtlasOptions = {
 	["AtlasSortBy"] = 4,
+	["AtlasDontShowInfo"] = true,
 	["AtlasCtrl"] = false,
 	["AtlasAlpha"] = 1,
 	["AtlasButtonRadius"] = 78,
@@ -12,9 +13,18 @@ AtlasOptions = {
 	["AtlasButtonShown"] = false,
 	["AtlasVersion"] = "1.18.2",
 	["AtlasButtonPosition"] = 26,
-	["AtlasRightClick"] = false,
-	["AtlasZone"] = 1,
-	["AtlasLocked"] = false,
-	["AtlasBossDesc"] = true,
 	["AtlasBossDescScale"] = 0.8999999761581421,
+	["AtlasZone"] = 1,
+	["AtlasBossDesc"] = true,
+	["AtlasLocked"] = false,
+	["AtlasRightClick"] = false,
+}
+AtlasDB = {
+	["profileKeys"] = {
+		["Pezzer - Burning Blade"] = "Pezzer - Burning Blade",
+	},
+	["profiles"] = {
+		["Pezzer - Burning Blade"] = {
+		},
+	},
 }
