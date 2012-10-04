c7edaac6c5db9155f0b0c8b08bf5aef39ee23b41
Spence <ImNotJimboJones@mac.com>
2012-10-04 16:26:56 -0400

    Reset damage meters and switched to current segment

--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/Skada.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/Skada.lua
@@ -7,54 +7,53 @@ SkadaDB = {
 		["Default"] = {
 			["showtotals"] = true,
 			["modules"] = {
-				["ccignoremaintanks"] = true,
 				["ccannounce"] = true,
+				["ccignoremaintanks"] = true,
 			},
 			["windows"] = {
 				{
 					["barheight"] = 16,
 					["classicons"] = false,
 					["barslocked"] = true,
-					["set"] = "total",
+					["background"] = {
+						["height"] = 121.1332244873047,
+						["color"] = {
+							["a"] = 0,
+							["b"] = 0.5019607843137255,
+						},
+					},
 					["y"] = 3.000383377075195,
 					["x"] = 356.710693359375,
 					["title"] = {
 						["color"] = {
 							["a"] = 0,
-							["b"] = 0.3019607843137255,
-							["g"] = 0.1019607843137255,
 							["r"] = 0.1019607843137255,
+							["g"] = 0.1019607843137255,
+							["b"] = 0.3019607843137255,
 						},
 						["font"] = "Arial Narrow",
 						["fontsize"] = 12,
 						["height"] = 20,
 						["texture"] = "Minimalist",
 					},
-					["point"] = "BOTTOM",
 					["barbgcolor"] = {
 						["a"] = 0.6000000238418579,
-						["b"] = 0.3019607843137255,
-						["g"] = 0.3019607843137255,
 						["r"] = 0.3019607843137255,
+						["g"] = 0.3019607843137255,
+						["b"] = 0.3019607843137255,
 					},
-					["mode"] = "Healing",
+					["barfont"] = "Arial Narrow",
 					["bartexture"] = "Minimalist",
 					["barwidth"] = 210.1308898925781,
 					["barspacing"] = 1,
-					["barfont"] = "Arial Narrow",
+					["point"] = "BOTTOM",
 					["barcolor"] = {
-						["b"] = 0.7490196078431373,
-						["g"] = 0.7490196078431373,
 						["r"] = 0.7490196078431373,
+						["g"] = 0.7490196078431373,
+						["b"] = 0.7490196078431373,
 					},
 					["barfontsize"] = 13,
-					["background"] = {
-						["color"] = {
-							["a"] = 0,
-							["b"] = 0.5019607843137255,
-						},
-						["height"] = 121.1332244873047,
-					},
+					["mode"] = "Damage taken",
 				}, -- [1]
 			},
 			["icon"] = {
