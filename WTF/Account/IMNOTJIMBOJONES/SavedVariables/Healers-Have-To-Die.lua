44eed48c3abe4980c4a440eba9e7a2dfa18823f8
Spence <ImNotJimboJones@mac.com>
2012-10-06 16:50:46 -0400

    No longer showing alerts when healers take damage

---
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/Healers-Have-To-Die.lua
@@ -0,0 +1,15 @@
+
+Healers_Have_To_Die = {
+	["namespaces"] = {
+		["Announcer"] = {
+		},
+		["NPH"] = {
+		},
+	},
+	["global"] = {
+		["HealerUnderAttackAlerts"] = false,
+	},
+	["profileKeys"] = {
+		["Pezzer - Burning Blade"] = "Pezzer - Burning Blade",
+	},
+}
