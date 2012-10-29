3c9c1dc53c8cb9e332934a023a0c9a1d41a6dddf
Spence <ImNotJimboJones@mac.com>
2012-10-29 18:31:59 -0400

    Locked mini zone map

--- WTF/Account/IMNOTJIMBOJONES/Burning Blade/Pezzer/SavedVariables/Blizzard_BattlefieldMinimap.lua
+++ WTF/Account/IMNOTJIMBOJONES/Burning Blade/Pezzer/SavedVariables/Blizzard_BattlefieldMinimap.lua
@@ -1,10 +1,10 @@
 
 BattlefieldMinimapOptions = {
-	["locked"] = false,
+	["locked"] = true,
 	["opacity"] = 0.4599999785423279,
+	["showPlayers"] = true,
 	["position"] = {
 		["y"] = 931.33984375,
 		["x"] = 63.64056396484375,
 	},
-	["showPlayers"] = true,
 }
