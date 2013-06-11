4ef562d28163bd09ff27a0bee84d14c059420602
Spence DiNicolantonio <ImNotJimboJones@mac.com>
2013-06-10 22:50:35 -0400

    Repositioned achievement alerts

--- WTF/Account/IMNOTJIMBOJONES/SavedVariables/xanachievementmover.lua
+++ WTF/Account/IMNOTJIMBOJONES/SavedVariables/xanachievementmover.lua
@@ -1,15 +1,15 @@
 
 XanAM_DB = {
-	["xanAchievementMover_Anchor"] = {
-		["yOfs"] = -179.9998321533203,
-		["xOfs"] = 0.0007221692358143628,
-		["point"] = "TOP",
-		["relativePoint"] = "TOP",
-	},
 	["xanAchievementMover_Ach1"] = {
 		["yOfs"] = -44.00000381469727,
 		["xOfs"] = 0,
 		["point"] = "CENTER",
 		["relativePoint"] = "BOTTOM",
+	},
+	["xanAchievementMover_Anchor"] = {
+		["yOfs"] = -234.9997863769531,
+		["xOfs"] = -284.9977722167969,
+		["point"] = "TOPRIGHT",
+		["relativePoint"] = "TOPRIGHT",
 	},
 }
